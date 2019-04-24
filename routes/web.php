91041974d4bf624e922670ff8af7590d1d7c9c6e
adansuku <adangrx@gmail.com>
2019-04-24 11:10:42 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,19 +16,10 @@ Route::get('/', function () {
 });
 
 Route::get('admin/idiomas', 'ModelosController@idiomas')->name('admin.idiomas');
-Route::get('admin/deleteidiomas/{id}', 'ModelosController@deletetatuajes');
-
 Route::get('admin/tatuajes', 'ModelosController@tatuajes')->name('admin.tatuajes');
-Route::get('admin/deletetatuajes/{id}', 'ModelosController@deletetatuajes');
-
 Route::get('admin/cicatrices', 'ModelosController@cicatrices')->name('admin.cicatrices');
-Route::get('admin/deletecicatrices/{id}', 'ModelosController@deletecicatrices');
-
 Route::get('admin/piercings', 'ModelosController@piercings')->name('admin.piercings');
-Route::get('admin/deletepiercings/{id}', 'ModelosController@deletepiercings');
-
 Route::get('admin/deportes', 'ModelosController@deportes')->name('admin.deportes');
-Route::get('admin/deletedeportes/{id}', 'ModelosController@deletedeportes');
 
     Route::post('modelos/pdf', ['uses' =>'ModelosController@pdf']);
     Route::post('package/api', ['uses' =>'ModelosController@package']);
